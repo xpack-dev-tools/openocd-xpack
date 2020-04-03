28dfd9ae5d87869672b926b01072e197c55acee2
Liviu Ionescu <ilg@livius.net>
2020-04-03 15:15:07 +0300

    scripts/common-functions: remove docker_images()

--- scripts/common-functions-source.sh
+++ scripts/common-functions-source.sh
@@ -14,15 +14,4 @@
 
 # -----------------------------------------------------------------------------
 
-function docker_images()
-{
-  # The names of the Docker images used for the build.
-  # docker run --interactive --tty ilegeul/centos:6-xbb-v2.1
-  docker_linux64_image=${docker_linux64_image:-"ilegeul/centos:6-xbb-v2.2"}
-  docker_linux32_image=${docker_linux32_image:-"ilegeul/centos32:6-xbb-v2.2"}
-  docker_linux_arm64_image=${docker_linux_arm64_image:-"ilegeul/ubuntu:arm64-16.04-xbb-v1.1"}
-  docker_linux_arm32_image=${docker_linux_arm32_image:-"ilegeul/ubuntu:armhf-16.04-xbb-v1.1"}
-}
-
-
 # -----------------------------------------------------------------------------
