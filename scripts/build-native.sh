43ccead9708fda0352359cea2a8b6c7f3737c0c2
Liviu Ionescu <ilg@livius.net>
2020-07-09 17:19:09 +0300

    build-native: use _versions

--- scripts/build-native.sh
+++ scripts/build-native.sh
@@ -52,10 +52,14 @@ script_folder_name="$(basename "${script_folder_path}")"
 echo
 echo "xPack OpenOCD native build script."
 
-echo
 host_functions_script_path="${script_folder_path}/helper/host-functions-source.sh"
-echo "Host helper functions source script: \"${host_functions_script_path}\"."
 source "${host_functions_script_path}"
+
+common_functions_script_path="${script_folder_path}/common-functions-source.sh"
+source "${common_functions_script_path}"
+
+defines_script_path="${script_folder_path}/defs-source.sh"
+source "${defines_script_path}"
 
 host_detect
 
@@ -64,14 +68,33 @@ host_detect
 help_message="    bash $0 [--win] [--debug] [--develop] [--jobs N] [--help] [clean|cleanlibs|cleanall]"
 host_native_options "${help_message}" $@
 
+echo
+echo "Host helper functions source script: \"${host_functions_script_path}\"."
+echo "Common functions source script: \"${common_functions_script_path}\"."
+echo "Definitions source script: \"${defines_script_path}\"."
+
 # -----------------------------------------------------------------------------
 
 host_common
 
 prepare_xbb_env
+export TARGET_BITS="${HOST_BITS}"
+
 prepare_xbb_extras
 
 # -----------------------------------------------------------------------------
+
+common_helper_libs_functions_script_path="${script_folder_path}/helper/common-libs-functions-source.sh"
+echo "Common helper libs functions source script: \"${common_helper_libs_functions_script_path}\"."
+source "${common_helper_libs_functions_script_path}"
+
+common_functions_script_path="${script_folder_path}/common-functions-source.sh"
+echo "Common functions source script: \"${common_functions_script_path}\"."
+source "${common_functions_script_path}"
+
+common_versions_script_path="${script_folder_path}/common-versions-source.sh"
+echo "Common versions source script: \"${common_versions_script_path}\"."
+source "${common_versions_script_path}"
 
 common_libs_functions_script_path="${script_folder_path}/${COMMON_LIBS_FUNCTIONS_SCRIPT_NAME}"
 echo "Common libs functions source script: \"${common_libs_functions_script_path}\"."
@@ -83,67 +106,13 @@ source "${common_apps_functions_script_path}"
 
 # -----------------------------------------------------------------------------
 
-OPENOCD_PROJECT_NAME="openocd"
-OPENOCD_VERSION="0.10.0-13"
-
-OPENOCD_SRC_FOLDER_NAME=${OPENOCD_SRC_FOLDER_NAME:-"${OPENOCD_PROJECT_NAME}.git"}
-OPENOCD_GIT_URL=${OPENOCD_GIT_URL:-"https://github.com/xpack-dev-tools/openocd.git"}
-
-if [ "${IS_DEVELOP}" == "y" ]
-then
-  OPENOCD_GIT_BRANCH=${OPENOCD_GIT_BRANCH:-"xpack-develop"}
-else
-  OPENOCD_GIT_BRANCH=${OPENOCD_GIT_BRANCH:-"xpack"}
-fi
-
-OPENOCD_GIT_COMMIT=${OPENOCD_GIT_COMMIT:-""}
-
-# Used in the licenses folder.
-OPENOCD_FOLDER_NAME="openocd-${OPENOCD_VERSION}"
-
-# -----------------------------------------------------------------------------
-
-LIBUSB1_VERSION="1.0.22"
-LIBUSB0_VERSION="0.1.5"
-LIBUSB_W32_VERSION="1.2.6.0"
-LIBFTDI_VERSION="1.4"
-LIBICONV_VERSION="1.15"
-HIDAPI_VERSION="0.8.0-rc1"
-
-LIBFTDI_PATCH="libftdi1-${LIBFTDI_VERSION}-cmake-FindUSB1.patch"
-LIBUSB_W32_PATCH="libusb-win32-${LIBUSB_W32_VERSION}-mingw-w64.patch"
-
-# -----------------------------------------------------------------------------
-# Build dependent libraries.
-
-if true
-then
-
-  do_libusb1
-  if [ "${TARGET_PLATFORM}" == "win32" ]
-  then
-    do_libusb_w32
-  else
-    do_libusb0
-  fi
-
-  do_libftdi
-
-  do_libiconv
-
-  do_hidapi
-
-fi
-
-# -----------------------------------------------------------------------------
-
-do_openocd
-
-run_openocd
+prepare_versions
 
 # -----------------------------------------------------------------------------
 
 host_stop_timer
+
+host_notify_completed
 
 # Completed successfully.
 exit 0
