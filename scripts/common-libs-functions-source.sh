9e09738b78ca14dd4e57230833b4a855e0bc0dea
Liviu Ionescu <ilg@livius.net>
2021-09-12 00:31:31 +0300

    common-libs: try to patch libudev.so

--- scripts/common-libs-functions-source.sh
+++ scripts/common-libs-functions-source.sh
@@ -726,8 +726,9 @@ function copy_libudev_with_links()
       cp -v "${from_file_path}" "${dest_folder_path}"
     fi
 
-    # Hack to get libudev.so in line with the 'all rpath' policy.
+    # Hack to get libudev.so in line with the 'all rpath' policy,
+    # since on arm 32-bit it is checked.
     # Manually add $ORIGIN to libudev.so (fingers crossed!).
-    # run_verbose ${PATCHELF} --force-rpath --set-rpath "\$ORIGIN" "${dest_file_path}"
+    run_verbose ${PATCHELF} --force-rpath --set-rpath "\$ORIGIN" "${dest_file_path}"
   fi
 }
