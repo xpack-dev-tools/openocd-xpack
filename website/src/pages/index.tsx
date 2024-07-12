5089d89dc7c3cd5fd3a08bbe7517fcc0595229a7
Liviu Ionescu <ilg@livius.net>
2024-07-12 07:40:21 +0300

    website: add copyright notice to code

--- website/src/pages/index.tsx
+++ website/src/pages/index.tsx
@@ -1,3 +1,14 @@
+/*
+ * This file is part of the xPack project (http://xpack.github.io).
+ * Copyright (c) 2024 Liviu Ionescu. All rights reserved.
+ *
+ * Permission to use, copy, modify, and/or distribute this software
+ * for any purpose is hereby granted, under the terms of the MIT license.
+ *
+ * If a copy of the license was not distributed with this file, it can
+ * be obtained from https://opensource.org/licenses/MIT/.
+ */
+
 import React from 'react';
 import clsx from 'clsx';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
