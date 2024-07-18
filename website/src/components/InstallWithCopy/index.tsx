ade98b4a16ce020b0e119003e6313ef411667b00
Liviu Ionescu <ilg@livius.net>
2024-07-18 19:13:49 +0300

    Revert "website: remove unused InstallWithCopy"
    This reverts commit 0aa0db792439f9c0aec657a2d71cbbf95c8fb248.

---
+++ website/src/components/InstallWithCopy/index.tsx
@@ -0,0 +1,21 @@
+/*
+ * This file is part of the xPack project (http://xpack.github.io).
+ * Copyright (c) 2024 Liviu Ionescu. All rights reserved.
+ *
+ * Permission to use, copy, modify, and/or distribute this software
+ * for any purpose is hereby granted, under the terms of the MIT license.
+ *
+ * If a copy of the license was not distributed with this file, it can
+ * be obtained from https://opensource.org/licenses/MIT/.
+ */
+
+import React from 'react';
+import CodeBlock from '@theme/CodeBlock';
+
+export default function InstallWithCopy({ children }): JSX.Element {
+    return (
+        <CodeBlock>
+            {children}
+        </CodeBlock>
+    );
+}
